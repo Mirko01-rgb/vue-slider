1333e6b6ccc7b8746e5c16fcaf46fac7108b90de
Mirko <mirkosbaglia2001@gmail.com>
2021-04-07 15:34:45 +0200

    Controllo funzionamento pulsanti

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
 //ricreare lo slider visto in jquery, con tecnologia Vue
+function initVue(){
+ new Vue({
+   el: '#app',
+   data : {
+
+   },
+   methods:{
+    left : function(){
+      console.log('left');
+    },
+
+    right : function(){
+      console.log('right');
+    }
+   }
+
+  });
+}
+
+
+function init(){
+  initVue();
+}
+document.addEventListener('DOMContentLoaded', init);
